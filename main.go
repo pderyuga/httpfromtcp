fa9707330cedd1bd27e51d92293f37a27923b509
pderyuga <polina.deryuga@persuit.com>
2026-02-21 10:27:26 -0500

    feat: read messages file 8 bytes at a time

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+const filePath = "messages.txt"
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open(filePath)
+	if err != nil {
+		// Log the error and exit if the file cannot be read
+		log.Fatalf("Unable to open %s: %v", filePath, err)
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		log.Fatalf("Failed to get file info: %v", err)
+	}
+
+	fileSize := fileInfo.Size()
+
+	fmt.Printf("Reading data from %s\n", filePath)
+	fmt.Println("=====================================")
+
+	for fileSize > 0 {
+		bufferSize := 8
+		buffer := make([]byte, bufferSize)
+		count, err := file.Read(buffer)
+		if err != nil && err.Error() != "EOF" { // Check for actual errors, not just end-of-file
+			log.Fatal(err)
+		}
+		if err != nil && err.Error() == "EOF" {
+			os.Exit(0)
+		}
+		fileSize -= int64(bufferSize)
+
+		fmt.Printf("read: %s\n", string(buffer[:count]))
+	}
 }
