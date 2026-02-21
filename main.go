25e642f7d48d8c93af6691d58ee11bbbcd9e92df
pderyuga <polina.deryuga@persuit.com>
2026-02-21 11:39:41 -0500

    refactor: create function that reads lines from a tcp connection

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
@@ -17,43 +18,53 @@ func main() {
 	}
 	defer file.Close()
 
-	fileInfo, err := file.Stat()
-	if err != nil {
-		log.Fatalf("Failed to get file info: %v", err)
-	}
-
-	fileSize := fileInfo.Size()
+	channel := getLinesChannel(file)
 
 	fmt.Printf("Reading data from %s\n", filePath)
 	fmt.Println("=====================================")
 
-	currentLine := ""
+	for line := range channel {
+		fmt.Printf("read: %s\n", line)
+	}
+}
 
-	for fileSize > 0 {
-		bufferSize := 8
-		buffer := make([]byte, bufferSize)
-		count, err := file.Read(buffer)
-		if err != nil && err.Error() != "EOF" { // Check for actual errors, not just end-of-file
-			log.Fatal(err)
-		}
-		if err != nil && err.Error() == "EOF" {
-			os.Exit(0)
-		}
-		fileSize -= int64(bufferSize)
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	lines := make(chan string)
 
-		str := string(buffer[:count])
+	go func() {
+		defer file.Close()
+		defer close(lines)
 
-		parts := strings.Split(str, "\n")
-		for i, part := range parts {
-			if i < len(parts)-1 {
-				// Not the last part - complete line found
-				currentLine += part
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			} else {
-				// Last part - might be incomplete, accumulate for next iteration
-				currentLine += part
+		currentLine := ""
+
+		for {
+			bufferSize := 8
+			buffer := make([]byte, bufferSize)
+			count, err := file.Read(buffer)
+			if err != nil && err.Error() != "EOF" { // Check for actual errors, not just end-of-file
+				log.Fatal(err)
+			}
+			if err != nil && err.Error() == "EOF" {
+				break
+			}
+
+			str := string(buffer[:count])
+
+			parts := strings.Split(str, "\n")
+			for i, part := range parts {
+				if i < len(parts)-1 {
+					// last chunk before newline - add to current line, print  current line, and reset current line to empty string
+					currentLine += part
+					lines <- currentLine
+					currentLine = ""
+				} else {
+					// not the last chunk before the newline, add to current line
+					currentLine += part
+				}
 			}
 		}
-	}
+	}()
+
+	return lines
+
 }
